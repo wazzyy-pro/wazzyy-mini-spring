569468c0c712e40b7d96e1e599763b06228edbd2
wazzyy <1351670200@qq.com>
2025-08-20 19:15:40 +0800

    feat(core): 实现资源加载功能
    - 新增 Resource 接口定义资源访问方法
    - 实现 ClassPathResource、FileSystemResource 和 UrlResource 三个资源类
    - 添加 DefaultResourceLoader 类用于统一资源加载
    - 新增 ClassUtils 工具类提供类加载器相关方法

---
+++ src/main/java/minispring/core/io/ResourceLoader.java
@@ -0,0 +1,43 @@
+package minispring.core.io;
+
+/**
+ * @Description 资源加载器接口
+ * 定义了获取资源的统一方法
+ * @Author wazzyy
+ * @Date 2025/8/20
+ */
+public interface ResourceLoader {
+
+    /**
+     * Classpath URL前缀
+     */
+    String CLASSPATH_URL_PREFIX = "classpath:";
+
+    /**
+     * 文件系统URL前缀
+     */
+    String FILE_URL_PREFIX = "file:";
+
+    /**
+     * HTTP URL前缀
+     */
+    String HTTP_URL_PREFIX = "http:";
+
+    /**
+     * HTTPS URL前缀
+     */
+    String HTTPS_URL_PREFIX = "https:";
+
+    /**
+     * 获取资源
+     * @param location 资源位置
+     * @return 资源对象
+     */
+    Resource getResource(String location);
+
+    /**
+     * 获取类加载器
+     * @return 类加载器
+     */
+    ClassLoader getClassLoader();
+}
